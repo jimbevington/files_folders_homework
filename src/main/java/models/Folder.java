6b6e1a4b174df6408c2f64be729e72abf77354c2
Jim Bevington <jim.bevington@hotmail.com>
2018-03-13 19:05:46 +0000

    added properties, constructors and getters and setters for File and Folder

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,4 +1,29 @@
 package models;
 
+import java.util.Set;
+
 public class Folder {
+
+    private String name;
+    private Set<File> files;
+
+    public Folder(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(Set<File> files) {
+        this.files = files;
+    }
 }
