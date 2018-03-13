8213582fc3b9da5939ecbfc26748257897e5c32c
Jim Bevington <jim.bevington@hotmail.com>
2018-03-13 20:00:59 +0000

    added Owner class

---
+++ src/main/java/models/Owner.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.Set;
+
+public class Owner {
+
+    private int id;
+    private String name;
+    private String username;
+    private Set<Folder> folders;
+
+    public Owner() {
+    }
+
+    public Owner(String name, String username) {
+        this.name = name;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Set<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(Set<Folder> folders) {
+        this.folders = folders;
+    }
+}
